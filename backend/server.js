b1b0b46eb699a487b500ccd07227b7eec3e836b5
SamuelKelechi <kcfreshie@ymail.com>
2022-11-30 07:05:25 +0100

    Created Routes

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
-
+app.use('/crud', require('./routes/crudRoutes'))
 
 app.listen(port, () => {
     console.log(`server running on port ${port}`)
