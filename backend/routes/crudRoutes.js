5745725720a9897de1055b622b09725b90f2ab0a
SamuelKelechi <kcfreshie@ymail.com>
2023-01-17 03:38:59 +0100

    Controller added

--- backend/routes/crudRoutes.js
+++ backend/routes/crudRoutes.js
@@ -1,19 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const {getCruds} = require('../controllers/crudController')
+const {getCruds, postCruds, updateCruds, deleteCruds} = require('../controllers/crudController')
 
 router.get('/', getCruds)
 
-router.post('/', (req, res) => {
-    res.status(200).json({message: "Post Messages"})
-})
+router.post('/', postCruds)
 
-router.put('/:id', (req, res) => {
-    res.status(200).json({message: `Update Messages ${req.params.id}`})
-})
+router.put('/:id', updateCruds)
 
-router.delete('/:id', (req, res) => {
-    res.status(200).json({message:  `Delete Messages ${req.params.id}`})
-})
+router.delete('/:id', deleteCruds)
 
 module.exports = router
